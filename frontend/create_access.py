e89e7449b350eaf919991c540613532e3477bf9d
Dimitris Palyvos <palyvos@chalmers.se>
2020-02-13 14:36:29 +0100

    Clean up code
    	- Force output flush for scheduler
    	- Small improvements in create_acess.py
    	- Install vim in frontend container
    	- Update .gitignore
    	- Improvements in error handling in crawler.py
    	- Update .gitignore.

--- frontend/create_access.py
+++ frontend/create_access.py
@@ -11,7 +11,6 @@ def connect_to_db(table):
     port = 3306
     user = 'root'
     password = 'the-secret-pw'
-    dbase = table
     path = 'mysql://'+ user +':'+ password + '@'+ host +':' + str(port) + '/' + table
 
     if 'MYSQL_ROOT_PASSWORD' in os.environ:
@@ -20,32 +19,31 @@ def connect_to_db(table):
         user = "root"
         password = os.getenv('MYSQL_ROOT_PASSWORD')
         path = 'mysql://'+ user +':'+ password + '@'+ host +':' + str(port) + '/' + table
-        print(path)
+        print('db path: ', path)
     return dataset.connect(path)
 
 def create_user(username, password):
 
     db = connect_to_db("teams")
     table = db.create_table("registrations")
-
-    table = db["registrations"]
-    exist = table.find_one(username=username)
-    if exist:
-        print(exist)
-        #if e['username'] == str(username):
+    exists = table.find_one(username=username)
+    if exists:
+        print(exists)
         print( "credentials with name %s already exist" % username)
         return
-
-    # if exist:
-    #     return "User is already exist"
-    # else:
     table.insert(dict(username=username, password=password))
     print("User was created successfully")
     return
 
 if __name__ == '__main__':
     print("Creating credentials to have access to the benchmark backend. ")
-    print("INFO! Username should containe @ character")
+
+    print('Curret users in DB:')
+    for userData in connect_to_db('teams')['registrations']:
+        print(userData)
+
+    print("INFO! Username should contain @ character")
+
 
     username = input('Provide username: ').strip(" ")
     username = re.sub(r"[,;\"\'\n\t\s]*", "",username)
--- scheduler/crawler.py
+++ scheduler/crawler.py
@@ -1,5 +1,6 @@
 import requests
 import datetime
+import logging
 
 DOCKER_REGISTRY_V2 = 'https://hub.docker.com/v2/repositories'
 
@@ -10,17 +11,21 @@ class DockerCrawler:
         self.table = []
 
     def convert_time(self, s):
-        #myfmt = "%Y-%m-%d %H:%M"
         return datetime.datetime.strptime(s, '%Y-%m-%dT%H:%M:%S.%fZ').replace(microsecond=0).replace(second=0)
 
     def run(self, image):
         docker_hub_link = image.split('/')
 
         url = DOCKER_REGISTRY_V2 + '/%s/%s/tags/'%(docker_hub_link[0], docker_hub_link[1])
-        #print("requesting: ", url)
-        data = requests.get(url)
-        if data.status_code >= 400:
-            print("Can't access image. This image may be marked as private. Status: ", data.status_code)
+        logging.info('Retrieving image data: %s', url)
+        try:
+            data = requests.get(url)
+            logging.info('Status Code: %d', data.status_code)
+            logging.debug('Image data: %s', data.json())
+            if data.status_code != 200:
+                raise Exception('Invalid status code! Is the image public?')
+            last_updated = data.json()['results'][0]['last_updated']
+            return self.convert_time(last_updated)
+        except:
+            logging.error('Failed to access image!')
             return
-        last_updated = data.json()['results'][0]['last_updated']
-        return self.convert_time(last_updated)
